5f62e8d6199a5c17ecb334040ffebb0b994e4a72
Roman Lutz <rolutz@microsoft.com>
2020-03-10 23:01:21 -0700

    adjust wheel build paths
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>

--- perf/environment_setup.py
+++ perf/environment_setup.py
@@ -9,21 +9,23 @@ import time
 
 def build_package():
     print('removing build directory')
-    shutil.rmtree("build", True)
+    shutil.rmtree(os.path.join("fairlearn", "build"), True)
     print('removing fairlearn.egg-info')
-    shutil.rmtree("fairlearn.egg-info", True)
+    shutil.rmtree(os.path.join("fairlearn", "fairlearn.egg-info"), True)
     print('removing dist directory')
-    shutil.rmtree("dist", True)
+    shutil.rmtree(os.path.join("fairlearn", "dist"), True)
 
     print('running python setup.py bdist_wheel')
-    subprocess.Popen(["python", "setup.py", "bdist_wheel"], cwd=os.getcwd(), shell=True).wait()
-    for root, dirs, files in os.walk("dist"):
+    subprocess.Popen(["python", "setup.py", "bdist_wheel"],
+                     cwd=os.path.join(os.getcwd(), "fairlearn"),
+                     shell=True).wait()
+    for root, dirs, files in os.walk(os.path.join("fairlearn", "dist")):
         for file in files:
             if file.endswith(".whl"):
                 print("Found wheel {}".format(file))
                 # change wheel name to be unique for every run
-                src = os.path.join("dist", file)
-                dst = os.path.join("dist", "fairlearn-v{}-py3-none-any.whl"
+                src = os.path.join("fairlearn", "dist", file)
+                dst = os.path.join("fairlearn", "dist", "fairlearn-v{}-py3-none-any.whl"
                                    .format(time.time()))
                 shutil.copy(src, dst)
                 return dst
